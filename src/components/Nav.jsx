af797adeb1c31cf5ba418ae1a75ecefae6c52a96
Brady Brown <brownb202@gmail.com>
2021-06-18 15:09:20 -0600

    added activestyles to nav bar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,20 +3,33 @@ import { NavLink } from 'react-router-dom'
 
 const Nav= () => {
 
+  const activeStyle = {
+    color: 'white',
+    backgroundColor: 'cadetblue',
+    // borderTop: 'groove black 1px',
+    // borderLeft: 'groove black 1px',
+    // borderRight: 'groove black 1px',
+    // borderBottom: 'none',
+    // borderRadius: '5px 5px 0px 0px'
+  }
+
   return (
     <nav>
-      <ul>
-        <li className='nav-item'>
+      <ul className='nav-ul'>
+        <li className='nav-li'>
           <NavLink
             to='/'
+            exact
+            activeStyle={activeStyle}
             className='nav-link'
             >
               Home
           </NavLink>
         </li>
-        <li className='nav-item'>
+        <li className='nav-li'>
           <NavLink
             to='/about'
+            activeStyle={activeStyle}
             className='nav-link'
             >
               About
