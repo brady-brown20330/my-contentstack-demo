3dcd073b5373ce7513347c517509ebcd5f4a7aca
Brady Brown <brownb202@gmail.com>
2021-06-18 16:52:45 -0600

    blog page now supports multiple paragraphs and headers

--- src/components/BlogPostTile.jsx
+++ src/components/BlogPostTile.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 
 const BlogPostTile = (props) => {
-  let copyPreview = props.post.blog_post_body.copy.substring(0, 50)
+  // let copyPreview = props.post.blog_post_body.copy.substring(0, 50)
   return (
     <div className='tile'>
       <img className='tile-img' src={props.post.banner.banner_image.url} />
       <div className='tile-content-container'>
         <h3 className='tile-title'>{props.post.banner.blog_title}</h3>
-        <div className='tile-copy-preview'>{copyPreview}...</div>
+        {/* <div className='tile-copy-preview'>{copyPreview}...</div> */}
         {/* Could this a just be a NavLink? */}
         <a className='tile-link' href={props.post.url}>Read More...</a>
       </div>
--- src/components/SingleBlogPost.jsx
+++ src/components/SingleBlogPost.jsx
@@ -2,30 +2,36 @@ import React from 'react'
 
 const SingleBlogPost = (props) => {
   let url = window.location.pathname
-
+  
   let post = props.blogPosts.filter((post, i) => {
     return props.blogPosts[i].url === url
   })
-
-  console.log('props in the single post: ', post)
+  console.log('One Single Blog Post: ', post[0])
+  
   if (!post[0]) return <h1>Error 404!</h1>
 
   return (
     <div className='blog-page'>
       <div className='banner' 
            style={{
-             backgroundImage: `url(${post[0].banner.banner_image.url})`,
-             backgroundSize: '100% auto',
-             backgroundPosition: 'center'
-            //  backgroundRepeat: 'no-repeat'
-            // objectFit: 'cover'
-
-             
-      }}>
+             backgroundImage: `url(${post[0].banner.banner_image.url})`}}>
         <h1 className='blog-title'>{post[0].banner.blog_title}</h1>
-        {/* <img className='banner-img' src={post[0].banner.banner_image.url}/> */}
       </div>
-      <p className='blog-copy'>{post[0].blog_post_body.copy}</p>
+      <div className='blog-copy'>
+        {
+          post[0].modular_blocks.map((item, i) => {
+            console.log(post[0].modular_blocks[i])
+            if (post[0].modular_blocks[i]['paragraph']) {
+              return (
+                <p>{post[0].modular_blocks[i].paragraph.paragraph_text[0]}</p>
+              )
+            } else if (post[0].modular_blocks[i]['headers']) {
+              return (
+                <h2>{post[0].modular_blocks[i].headers.header}</h2>
+              )
+            }
+          })}
+      </div>
     </div>
   )
 }
