5bf06931c49d636e7842f454aa98630ff449adcb
Brady <brown@Bradys-MacBook-Pro.local>
2022-01-07 11:35:03 -0500

    undoing LP changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,24 +6,24 @@ import Axios from 'axios';
 const Header = (props) => {
   const [data, setData] = React.useState({});
 
-  const updateData = () => {
+  // const updateData = () => {
 
-    const fetchedData = Axios.get('https://cdn.contentstack.io/v3/content_types/header/entries/blta059543334da4463?api_key=blta80ffe6a4d78d787&access_token=&environment=production&locale=en-us')
-    setData(fetchedData);
-    console.log('the newness: ', fetchedData)
-  };
+  //   const fetchedData = Axios.get('https://cdn.contentstack.io/v3/content_types/header/entries/blta059543334da4463?api_key=blta80ffe6a4d78d787&access_token=&environment=production&locale=en-us')
+  //   setData(fetchedData);
+  //   console.log('the newness: ', fetchedData)
+  // };
 
-  React.useEffect(() => {
-    onEntryChange(updateData);
-  }, []);
+  // React.useEffect(() => {
+  //   onEntryChange(updateData);
+  // }, []);
 
-  // return (
-  //   <div className='header-container'>
-  //     <h1 className='header-title'>{props.title}</h1>
-  //     <div className='header-subtext'>{props.subtext}</div>
-  //     <Nav />
-  //   </div>
-  // )
+  return (
+    <div className='header-container'>
+      <h1 className='header-title'>{props.title}</h1>
+      <div className='header-subtext'>{props.subtext}</div>
+      <Nav />
+    </div>
+  )
 }
 
 export default Header;
